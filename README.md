dd58d1a51bfffc308a97b6b1bbd52b3963a07226
Seangongx <sean.gong814@gmail.com>
2023-07-11 18:49:46 +0200

    Install DGtal

---
+++ README.md
@@ -0,0 +1,1 @@
+Test DGtal installation and Update the project
--- helloworld.cpp
+++ helloworld.cpp
@@ -1,32 +1,43 @@
-#include <iostream>
-#include <Eigen/Dense>
+#include <DGtal/base/Common.h>
  
-//using Eigen::MatrixXd;
-using namespace Eigen;
-using namespace Eigen::internal;
-using namespace Eigen::Architecture;
- 
-using namespace std;
- 
-int main()
+int main(int argc, char** argv)
 {
-        cout<<"*******************1D-object****************"<<endl;
-        Vector4d v1;
-        v1<< 1,2,3,4;
-        cout<<"v1=\n"<<v1<<endl;
- 
-        VectorXd v2(3);
-        v2<<1,2,3;
-        cout<<"v2=\n"<<v2<<endl;
- 
-        Array4i v3;
-        v3<<1,2,3,4;
-        cout<<"v3=\n"<<v3<<endl;
- 
-        ArrayXf v4(3);
-        v4<<1,2,3;
-        cout<<"v4=\n"<<v4<<endl;
+  
+  DGtal::trace.info() << "Helloworld from DGtal ";
+  DGtal::trace.emphase() << "(version "<< DGTAL_VERSION << ")"<< std::endl;
+  
+  return 0;
 }
+
+// #include <iostream>
+// #include <Eigen/Dense>
+ 
+// //using Eigen::MatrixXd;
+// using namespace Eigen;
+// using namespace Eigen::internal;
+// using namespace Eigen::Architecture;
+ 
+// using namespace std;
+ 
+// int main()
+// {
+//         cout<<"*******************1D-object****************"<<endl;
+//         Vector4d v1;
+//         v1<< 1,2,3,4;
+//         cout<<"v1=\n"<<v1<<endl;
+ 
+//         VectorXd v2(3);
+//         v2<<1,2,3;
+//         cout<<"v2=\n"<<v2<<endl;
+ 
+//         Array4i v3;
+//         v3<<1,2,3,4;
+//         cout<<"v3=\n"<<v3<<endl;
+ 
+//         ArrayXf v4(3);
+//         v4<<1,2,3;
+//         cout<<"v4=\n"<<v4<<endl;
+// }
 
 // #include <boost/lambda/lambda.hpp>
 // #include <iostream>
