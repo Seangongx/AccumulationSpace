85ffc7be20ee603b7a3eeed7cdaee2e1d21a79f1
Seangongx <sean.gong814@gmail.com>
2023-07-24 04:40:13 +0200

    remove igonre files

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 
   DGtal::trace.info() << "Helloworld from DGtal ";
   DGtal::trace.emphase() << "(version " << DGTAL_VERSION << ")" << std::endl;
-
+//
   return 0;
 }
 
