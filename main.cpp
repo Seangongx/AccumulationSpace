b5a18525883cbdca2baa900572dd0d74257f1f19
Seangongx <xun.gong@telecom-paris.fr>
2023-10-02 17:41:37 +0200

    Delete redundant files

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include "DGtal/shapes/Mesh.h"
 #include "DGtal/shapes/MeshHelpers.h"
 #include "DGtal/io/viewers/Viewer3D.h"
+#include <DGtal/io/readers/MeshReader.h>
+#include <DGtal/io/writers/MeshWriter.h>
+#include "NormalAccumulator.h"
+
+#include "CLI11.hpp"
 
 using namespace std;
 using namespace DGtal;
@@ -9,34 +14,66 @@ using namespace DGtal::Z3i;
 
 int main(int argc, char **argv)
 {
-  typedef TriangulatedSurface<RealPoint> TriMesh;
-  typedef Mesh<RealPoint> ViewMesh;
+  string inputFile;
+  double inputRadius{5.0};
+  string inputTypeStat{"min"};
 
-  // Creates two triangles glued together.
-  TriMesh tmesh;
-  tmesh.addVertex(RealPoint(0, 0, 0));
-  tmesh.addVertex(RealPoint(1, 0, 0));
-  tmesh.addVertex(RealPoint(0, 1, 0));
-  tmesh.addVertex(RealPoint(1, 1, 1));
-  tmesh.addTriangle(0, 1, 2);
-  tmesh.addTriangle(2, 1, 3);
-  tmesh.build();
+  // parse command line using CLI ----------------------------------------------
+  CLI::App app;
+  app.description("Compute mesh accumulation from a mesh and compute the radius (median value) "
+                  "of all faces participating to the accumulation");
+  app.add_option("-i,--input,1", inputFile, "input mesh.")
+      ->required()
+      ->check(CLI::ExistingFile);
 
-  // Convert it to a mesh
-  ViewMesh mesh;
-  MeshHelpers::triangulatedSurface2Mesh(tmesh, mesh);
+  app.add_option("--radius,-r", inputRadius, "radius of accumulation analysis.", true);
+  app.add_option("--radiusEstimator,-e", inputTypeStat, "use: {min (default), max, mean, median} to estimate the radius", true)
+      ->check(CLI::IsMember({"max", "min", "mean", "median"}));
+  app.get_formatter()->column_width(40);
+  CLI11_PARSE(app, argc, argv);
+  // END parse command line using CLI ----------------------------------------------
 
-  // View it
-  QApplication application(argc, argv);
-  Viewer3D<> viewer;
-  viewer.show();
-  viewer.setLineColor(Color(150, 0, 0, 254));
-  viewer << mesh;
-  viewer << Viewer3D<>::updateDisplay;
-  application.exec();
+  // 1) Reading input mesh
+  Mesh<Z3i::RealPoint> tempMesh(true);
+  tempMesh << inputFile;
+
+  NormalAccumulator na(inputRadius, inputTypeStat);
 
   return 0;
 }
+
+// RealVector a(1, 1, 1);
+// RealVector b(2, 4, 6);
+// RealVector c = a.crossProduct(b);
+// cout << a << endl;
+// cout << b << endl;
+// cout << c << endl;
+
+// typedef TriangulatedSurface<RealPoint> TriMesh;
+// typedef Mesh<RealPoint> ViewMesh;
+
+// // Creates two triangles glued together.
+// TriMesh tmesh;
+// tmesh.addVertex(RealPoint(0, 0, 0));
+// tmesh.addVertex(RealPoint(1, 0, 0));
+// tmesh.addVertex(RealPoint(0, 1, 0));
+// tmesh.addVertex(RealPoint(1, 1, 1));
+// tmesh.addTriangle(0, 1, 2);
+// tmesh.addTriangle(2, 1, 3);
+// tmesh.build();
+
+// // Convert it to a mesh
+// ViewMesh mesh;
+// MeshHelpers::triangulatedSurface2Mesh(tmesh, mesh);
+
+// // View it
+// QApplication application(argc, argv);
+// Viewer3D<> viewer;
+// viewer.show();
+// viewer.setLineColor(Color(150, 0, 0, 254));
+// viewer << mesh;
+// viewer << Viewer3D<>::updateDisplay;
+// application.exec();
 
 // QApplication application(argc,argv);
 // Viewer3D<> viewer;
--- showAccMesh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/adam/Desktop/AccumulationSpace
-3dVolViewer /home/adam/Desktop/AccumulationSpace/accumulation.longvol  -m 5 -t 50  --displayMesh /home/adam/Desktop/AccumulationSpace/samples/am_beech2-3.off
